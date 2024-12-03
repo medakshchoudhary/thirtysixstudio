5a5dc4eb899ba1556547b167d5d4690ab5076707
Daksh Choudhary <ch.daksh01@gmail.com>
2024-12-03 23:35:27 +0530

    canvas working for images

--- src/Components/Canvas.jsx
+++ src/Components/Canvas.jsx
@@ -1,20 +1,36 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import canvasImages from "../canvasImages";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 function Canvas() {
+
+    const [index, setIndex] = useState({value : 0})
     const canvasRef = useRef(null);
+
+    useGSAP(()=>{
+        gsap.to(index, {
+            value: 149,
+            duration: 3,
+            ease: "linear",
+            repeat: -1,
+            onUpdate: () => {
+                setIndex({value : Math.round(index.value)})
+            }
+        })
+    })
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
         const img = new Image();
-        img.src = canvasImages[0];
+        img.src = canvasImages[index.value];
         img.onload = function() {
             canvas.width = img.width;
             canvas.height = img.height;
             ctx.drawImage(img, 0, 0);
         };
-    }, []);
+    }, [index]);
 
     return (
         <div>
